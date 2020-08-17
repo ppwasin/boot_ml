8733066a1da5cad9c09f04674ebad1a9269b3cff
wasin.p <wasin.p@getthailand.com>
2020-08-17 08:58:11 +0700

    getting start with iris

--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    iris = datasets.load_iris()
+    digits = datasets.load_digits()
+    print(digits.data)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
